fd43de7ae575c1b60ea9ed059aa2b4b6fc082a5b
AnganSamadder <rio.angan@gmail.com>
2026-01-25 15:01:37 -0500

    Initial Commit

---
+++ WaniKani/WaniKaniApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaniKaniApp.swift
+//  WaniKani
+//
+//  Created by Angan Samadder on 1/25/26.
+//
+
+import SwiftUI
+
+@main
+struct WaniKaniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
