2685614c7c97c3fd4079a8e173863f2a6663ea67
AnganSamadder <rio.angan@gmail.com>
2026-01-27 17:59:04 -0500

    fix: Disable animations during UI tests to prevent timeouts

--- WaniKani/App/WaniKaniApp.swift
+++ WaniKani/App/WaniKaniApp.swift
@@ -6,9 +6,20 @@
 //
 
 import SwiftUI
+#if canImport(UIKit)
+import UIKit
+#endif
 
 @main
 struct WaniKaniApp: App {
+    init() {
+        #if canImport(UIKit)
+        if ProcessInfo.processInfo.arguments.contains("-UITesting") {
+            UIView.setAnimationsEnabled(false)
+        }
+        #endif
+    }
+
     var body: some Scene {
         WindowGroup {
             RootView()
